340d7d96fa810d8ec0d02e2c74f0d99de9d3fbc7
krishna kumar <79894493+krishna73220@users.noreply.github.com>
2023-07-12 16:30:36 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,61 +1,64 @@
-let cut=document.querySelector(".cut");
-let togal=document.querySelector(".togal");
+let cut = document.querySelector(".cut");
+let togal = document.querySelector(".togal");
 
-let aside=document.querySelector(".aside");
+let aside = document.querySelector(".aside");
 
-togal.addEventListener("click",function(){
+togal.addEventListener("click", function () {
 
-  aside.style.width='240px';
+    aside.style.width = '240px';
 });
 
-cut.addEventListener("click",function(){
-    aside.style.width='0px'
+cut.addEventListener("click", function () {
+    aside.style.width = '0px'
 })
 
-let k1=document.querySelectorAll(".k1")
+let k1 = document.querySelectorAll(".k1")
 
 
-let about_headList= document.querySelectorAll(".about_headList");
+let about_headList = document.querySelectorAll(".about_headList");
 
 k1[0].classList.remove("active");
 
 
-about_headList[0].addEventListener("click",function(){
+about_headList[0].addEventListener("click", function () {
 
-    k1.forEach( item=>{
+    k1.forEach(item => {
         item.classList.add("active")
     });
     k1[0].classList.remove("active");
-    
-    about_headList.forEach( item=>{
+
+    about_headList.forEach(item => {
         item.classList.remove("activelink")
     });
     about_headList[0].classList.add("activelink");
 })
-about_headList[1].addEventListener("click",function(){
+about_headList[1].addEventListener("click", function () {
 
-    k1.forEach( item=>{
+    k1.forEach(item => {
         item.classList.add("active")
     });
     k1[1].classList.remove("active");
 
-    about_headList.forEach( item=>{
+    about_headList.forEach(item => {
         item.classList.remove("activelink")
     });
     about_headList[1].classList.add("activelink");
 })
-about_headList[2].addEventListener("click",function(){
-  
-    k1.forEach( item=>{
+about_headList[2].addEventListener("click", function () {
+
+    k1.forEach(item => {
         item.classList.add("active")
     });
     k1[2].classList.remove("active");
 
-    about_headList.forEach( item=>{
+    about_headList.forEach(item => {
         item.classList.remove("activelink")
     });
     about_headList[2].classList.add("activelink");
 })
+
+
+
 
 let fleg = 1;
 
@@ -72,11 +75,21 @@ function inputFun() {
     }
     else{
         document.querySelector(".msgform").textContent="Message sent successfully";
-//         form.reset()
+        // form.reset()
          return true;
         
     }
 
 }
 
+let seeMore=document.querySelectorAll('.seeMore')
 
+for (const item of seeMore){
+document.querySelector('.abtn').addEventListener('click',()=>{
+
+        
+    item.style.display="block"
+    document.querySelector('.abtn').style.display="none"
+
+})
+}
